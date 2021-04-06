47633ee13a148076edf2c4ee4c3ba6fd798e1fbe
barsanges <barsanges@users.noreply.github.com>
2021-04-06 22:19:39 +0200

    Add a module 'Tile'.

---
+++ src/Kriegspiel/Game/Tile.hs
@@ -0,0 +1,15 @@
+{- |
+   Module      : Kriegspiel.Game.Tile
+   Copyright   : Copyright (C) 2021 barsanges
+   License     : Non-Profit Open Software License 3.0
+
+A tile of the board.
+-}
+
+module Kriegspiel.Game.Tile (
+  Tile(..)
+  ) where
+
+-- | A tile of the board.
+data Tile = Plain | Fortress | Mountain | Pass
+  deriving Eq
